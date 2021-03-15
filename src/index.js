79cb4b8488866a7eb4d1e57bdd1329b796581470
Nadiia <npodufala15@gmail.com>
2021-03-15 10:26:04 +0200

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	str = str.split("");
+	let newStr = str.length,
+    brack = [];
+
+	for (let i = 0; i < newStr; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if (str[i] == bracketsConfig[j][1] && brack[brack.length - 1] == bracketsConfig[j][0]) {
+				brack.pop();
+			} else if (str[i] == bracketsConfig[j][0]) {
+				brack.push(str[i]);
+			}
+		}
+	}
+	return brack.length == 0 && newStr % 2 == 0 ? true : false;
 }
